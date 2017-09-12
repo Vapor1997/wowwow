b0e07b024a250281b63cb680bfe4a8a0efcee41d
Vapor1997 <893201500@qq.com>
2017-09-12 23:31:20 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/wowwow/PengActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.android.wowwow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PengActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peng);
+    }
+}
